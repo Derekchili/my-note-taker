fe88828a0fea5f0e59498c1ca4a10869dc12ae00
derek <derekchili@gmail.com>
2023-04-25 19:58:04 -0700

    updated files

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+const allRoutes = require('./controllers');
+app.use(allRoutes)
+
+app.listen(3000, ()=>{
+    console.log('listening on port 3000!')
+})
