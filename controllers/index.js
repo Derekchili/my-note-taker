29eb72e780b5182e98a55b3e14f032d9a5dfeb0e
derek <derekchili@gmail.com>
2023-04-26 06:55:54 -0700

    udated js files

--- controllers/index.js
+++ controllers/index.js
@@ -6,3 +6,12 @@ router.get('/', (req, res)=>{
     res.sendFile(path.join(_dirname, '../public/index.html'))
 })
 
+router.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+
+router.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+module.exports = router;
---
+++ controllers/textControllers.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const uuid = require('uuid');
+
+router.get('/', (req, res)=> {
+    fs.readFile('')
+})
+
+
+
+module.exports = router;
